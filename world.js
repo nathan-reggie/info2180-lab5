8af42c060e1bd8ea8ac656c6f97cf7ead83542c3
nathansmith <nathan2012smith@gmail.com>
2023-11-20 19:27:23 -0500

    Finished Exercise 5

--- world.js
+++ world.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    const button = document.getElementById("lookup")
+    const countryButton = document.getElementById("lookup-countries")
+    const cityButton = document.getElementById("lookup-cities")
     const input = document.getElementById("country")
     const resultDiv = document.getElementById("result")
-    button.addEventListener("click", async ()=>{
-        inputVal = input.value
+    countryButton.addEventListener("click", async (event)=>{
+        event.preventDefault()
+        resultDiv.replaceChildren()
+        let inputVal = input.value
         const result = await fetch(`http://localhost:8888/info2180-lab5/world.php?country=${inputVal}`)
+        const data = await result.text()
+        resultDiv.innerHTML = data
+    })
+    cityButton.addEventListener("click", async (event)=>{
+        event.preventDefault()
+        resultDiv.replaceChildren()
+        let inputVal = input.value
+        const result = await fetch(`http://localhost:8888/info2180-lab5/world.php?country=${inputVal}&cities=${true}`)
         const data = await result.text()
         resultDiv.innerHTML = data
     })
--- world.php
+++ world.php
@@ -12,18 +12,38 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 //$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if ($_SERVER['REQUEST_METHOD']=== "GET") {
-  if(isset($_GET["country"])){
+  if(isset($_GET["country"]) and isset($_GET["cities"])){
     $country = $_GET["country"];
-    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $cities = true;
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities
+    JOIN countries ON cities.country_code = countries.code
+    WHERE countries.name LIKE '%$country%';");
   }
-  else {
-    $stmt = $conn->query("SELECT * FROM countries");
+  else if(isset($_GET["country"])){
+    $country = $_GET["country"];
+    $cities = false;
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
   }
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 ?>
 <table>
+  <?php if($cities === true):?>
+    <tr>
+    <th>Name</th>
+    <th>District</th>
+    <th>Population</th>
+    </tr>
+  <?php foreach ($results as $row):?>
+    <tr>
+      <td><?= $row["name"]; ?></td>
+      <td><?= $row["district"]; ?></td>
+      <td><?= $row["population"]; ?></td>
+    </tr>
+  <?php endforeach?>
+  <?php endif?>
+  <?php if($cities === false):?>
   <tr>
     <th>Name</th>
     <th>Continent</th>
@@ -38,4 +58,5 @@ if ($_SERVER['REQUEST_METHOD']=== "GET") {
       <td><?= $row["head_of_state"]; ?></td>
     </tr>
   <?php endforeach; ?>
+  <?php endif?>
 </table>
